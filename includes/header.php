cb724b18ea55e5a358366f9e0a2817d358074668
lobomaudani <lobomaudani05@gmail.com>
2025-10-02 09:49:32 -0300

    Mudanças

--- includes/header.php
+++ includes/header.php
@@ -1,6 +1,6 @@
 <header>
     <?php include 'logo.inc'; ?>
-    <?php include 'chamarHeader.php'; ?>
+
     <div class="search-bar" style="margin: 10px 0;">
         <form action="pesquisaProdutos.php" method="GET" class="d-flex" role="search">
             <input class="form-control me-2" type="search" name="q" 
@@ -11,10 +11,9 @@
             <button class="btn btn-outline-success" type="submit" style="padding: 6px 12px; border-radius: 4px;">Pesquisar</button>
         </form>
     </div>
-    
-    
-        
-    </div>
 
+    <div class="user-actions" id="user-actions">    
+
+    </div>
     
 </header>
